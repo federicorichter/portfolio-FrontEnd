6895ddadd494a360b153faba6c9e82ca536b0ab3
federicorichter <federicorichter123@gmail.com>
2022-11-25 11:30:25 -0300

    upheader

---
+++ AngularInicial/proyecto/src/app/components/upheader/upheader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upheader',
+  templateUrl: './upheader.component.html',
+  styleUrls: ['./upheader.component.css']
+})
+export class UpheaderComponent {
+
+}
